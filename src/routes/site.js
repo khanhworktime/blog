ec86131bdbbc55c2868da075c43723f9b7bce59f
khanhworktime <chikhanh5@gmail.com>
2022-01-29 01:48:41 +0700

    testing

--- src/routes/site.js
+++ src/routes/site.js
@@ -5,11 +5,8 @@ const siteController = require('../app/controllers/SiteController');
 
 siteController.index;
 
-router.use(
-    '/search',
-    siteController.search);
+router.use('/search', siteController.search);
 
-router.use(
-    '/', siteController.index);
+router.use('/', siteController.index);
 
 module.exports = router;
